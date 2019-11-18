a5d5e88d8e4fb1d52d1fe21d5d1faf91118d7831
Jo Jaramillo <jaramibr@oregonstate.edu>
2019-11-18 15:21:34 -0800

    Project Milestone One
    Copy of example for milestone 1

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ $(updateView)
 function updateView() {
     alert("Using jQuery!")
 }
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
+$(updateView)
+
+function updateView() {
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
+}

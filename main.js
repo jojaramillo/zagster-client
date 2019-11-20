06a556d51bdec6102bd07577fd64e6dde25cf448
Jo Jaramillo <jaramibr@oregonstate.edu>
2019-11-20 15:49:19 -0800

    Project Milestone 2
    Graph Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-$(updateView)
+$(updateGraph)
 
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
@@ -9,4 +9,28 @@ function updateView() {
 function updateRideCount(data) {
   numberOfRides = data.count
   $("h2#rideCount").html(numberOfRides)
+  alert(numberOfRides);
+  console.log(numberOfRides);
+}
+
+function updateGraph() {
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 }
